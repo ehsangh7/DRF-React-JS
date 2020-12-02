51637f37309aab44fe335757d66528e1731bbed2
unknown <ehsanghochagh@gamil.com>
2020-12-02 18:16:50 +0330

    fetching url and reciving data from API

--- React/blogapi/src/App.js
+++ React/blogapi/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class connectionExample extends Component {
+  componentDidMount(){
+    const apiUrl = 'http://127.0.0.1:8000/api/';
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+  }
+  render() {
+    return (
+      <div>
+        Example Connection
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connectionExample;
+
