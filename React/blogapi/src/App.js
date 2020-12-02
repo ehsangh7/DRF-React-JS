729b3273f3d2ec644fe86afa9f2a1997e05c159f
unknown <ehsanghochagh@gamil.com>
2020-12-02 18:34:27 +0330

    modified App.js and implement Theory

--- React/blogapi/src/App.js
+++ React/blogapi/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react'
+import React ,{ useState, useEffect } from 'react';
+import './App.css'
+import Posts from './components/PostLoading'
+import PostLoadingComponent from './components/PostLoading';
 
-export class connectionExample extends Component {
-  componentDidMount(){
-    const apiUrl = 'http://127.0.0.1:8000/api/';
+
+const App = () => {
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/`;
     fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => data.json)
+        .then((posts) => {
+          setAppState({ loading:false, posts: posts })
+        })
+  }, [setAppState])
 
-  }
-  render() {
-    return (
-      <div>
-        Example Connection
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      <h1>Lastest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+      
+    </div>
+  )
 }
 
-export default connectionExample;
-
+export default App
