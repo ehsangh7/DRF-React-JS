0d23198b31fc521404bc715d83e1f07828ff9a51
unknown <ehsanghochagh@gamil.com>
2020-12-03 21:43:52 +0330

    handle Permissions of rest framework with built in setting

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -127,11 +127,18 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
 
+# Permissions :
+# AllowAny 
+# IsAuthenticated
+# IsAdminUser
+# IsAuthenticaredOrReadOnly
+
+
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000"
+    "http://localhost:3000"
 ]
