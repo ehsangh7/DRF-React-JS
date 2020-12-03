454e73e309684879c04aad31f651eddc62639e93
unknown <ehsanghochagh@gamil.com>
2020-12-03 23:54:09 +0330

    create view level of permission by create custom permission

--- Django/blog_api/views.py
+++ Django/blog_api/views.py
@@ -1,18 +1,40 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions 
+import 
+ IsAdminUser, 
+ DjangoModelPermissions, 
+ BasePermission, 
+ IsAuthenticatedOrReadOnly, 
+ SAFE_METHODS
+
+
 # Create your views here.
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only'
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+
+
+class PostList(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
     
     
 
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
