ff80ad93c1719086892de7ef79e5ea0b8f05a86f
unknown <ehsanghochagh@gamil.com>
2020-12-06 19:09:59 +0330

    change TOKEN Life time

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -156,8 +156,8 @@ AUTH_USER_MODEL = "users.NewUser"
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
