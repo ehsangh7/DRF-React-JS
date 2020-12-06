5c4929de2667395ca3c57f41fd7e05b537ce4b70
unknown <ehsanghochagh@gamil.com>
2020-12-06 19:25:35 +0330

    add view function for token black list

--- Django/users/views.py
+++ Django/users/views.py
@@ -20,15 +20,15 @@ class CustomUserCreate(APIView):
         return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class BlacklistTokenUpdateView(APIView):
-#     permission_classes = [AllowAny]
-#     authentication_classes = ()
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
+  
 
-#     def post(self, request):
-#         try:
-#             refresh_token = request.data["refresh_token"]
-#             token = RefreshToken(refresh_token)
-#             token.blacklist()
-#             return Response(status=status.HTTP_205_RESET_CONTENT)
-#         except Exception as e:
-#             return Response(status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
