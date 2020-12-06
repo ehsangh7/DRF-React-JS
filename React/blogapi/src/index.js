0915d00bbcd779599272e77780f64b87a23fd0c0
unknown <ehsanghochagh@gamil.com>
2020-12-06 18:36:15 +0330

    Routing login

--- React/blogapi/src/index.js
+++ React/blogapi/src/index.js
@@ -7,6 +7,9 @@ import { Route ,BrowserRouter as Router, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/register';
+import Login from './components/login';
+// import Logout from './components/logout';
+
 
 
 
@@ -18,6 +21,8 @@ ReactDOM.render(
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/logout" component={Logout} /> */}
 
         </Switch>
         <Footer />
