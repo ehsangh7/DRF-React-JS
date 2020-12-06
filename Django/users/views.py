b906289405aba23132a2e76efa841c19e3b7b228
unknown <ehsanghochagh@gamil.com>
2020-12-06 17:53:43 +0330

    modified views.py

--- Django/users/views.py
+++ Django/users/views.py
@@ -2,7 +2,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CustomUserSerializer
+from .serializers import RegisterUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 
