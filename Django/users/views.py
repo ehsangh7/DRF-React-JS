7a615fdeb8f99698a293c07e12a1cb0056bf9019
unknown <ehsanghochagh@gamil.com>
2020-12-07 10:54:45 +0330

    modified views.py for som reason

--- Django/users/views.py
+++ Django/users/views.py
@@ -2,7 +2,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import RegisterUserSerializer
+from .serializers import CustomUserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 
@@ -10,19 +10,19 @@ from rest_framework.permissions import AllowAny
 class CustomUserCreate(APIView):
     permission_classes = [AllowAny]
 
-    def post(self, request):
-        reg_serializer = RegisterUserSerializer(data=request.data)
-        if reg_serializer.is_valid():
-            newuser = reg_serializer.save()
-            if newuser:
-                
-                return Response(status=status.HTTP_201_CREATED)
-        return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class BlacklistTokenView(APIView):
+class BlacklistTokenUpdateView(APIView):
     permission_classes = [AllowAny]
-  
+    authentication_classes = ()
 
     def post(self, request):
         try:
