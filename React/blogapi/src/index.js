cb14c7bd5cfa9acc18519f95deca0ee1fdf2b200
unknown <ehsanghochagh@gamil.com>
2020-12-07 11:08:55 +0330

    add logout page to index

--- React/blogapi/src/index.js
+++ React/blogapi/src/index.js
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/register';
 import Login from './components/login';
-// import Logout from './components/logout';
+import Logout from './components/logout';
 
 
 
@@ -22,7 +22,7 @@ ReactDOM.render(
           <Route exact path="/" component={App} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
-          {/* <Route exact path="/logout" component={Logout} /> */}
+          <Route exact path="/logout" component={Logout} />
 
         </Switch>
         <Footer />
