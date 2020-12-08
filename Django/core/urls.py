0a17b9df0b1a5a77c554b6185661155618025dcc
unknown <ehsanghochagh@gamil.com>
2020-12-08 13:15:46 +0330

    add docs Urls from restframwork documentation

--- Django/core/urls.py
+++ Django/core/urls.py
@@ -20,6 +20,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
@@ -30,6 +31,7 @@ urlpatterns = [
     path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('blog.urls', namespace='blog')),
+    path('docs/', include_docs_urls(title='BlogAPI')),
     path('schema/', get_schema_view(
         title="BlogAPI",
         description="API for the BlogAPI",
