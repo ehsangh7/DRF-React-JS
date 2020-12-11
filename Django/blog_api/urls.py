8b5a4fa0ea7228563550830f466e275211691a45
unknown <ehsanghochagh@gamil.com>
2020-12-11 23:45:35 +0330

    create CRUD URLs

--- Django/blog_api/urls.py
+++ Django/blog_api/urls.py
@@ -1,4 +1,6 @@
-from .views import PostList, PostDetail, PostSearch, PostListDetailfilter
+from .views import (
+    PostList, PostDetail, PostListDetailfilter,
+    CreatePost, AdminPostDetail, EditPost, DeletePost)
 #from rest_framework.routers import DefaultRouter
 from django.urls import path
 
@@ -9,7 +11,13 @@ app_name = 'blog_api'
 # urlpatterns = router.urls
 
 urlpatterns = [
-    path('posts/', PostDetail.as_view(), name='detailcreate'),
-    path('search/', PostListDetailfilter.as_view(), name='postsearch'),
-    path('', PostList.as_view(), name='listcreate'),
+    path('', PostList.as_view(), name='listpost'),
+    path('post/<str:pk>/', PostDetail.as_view(), name='detailpost'),
+    path('search/', PostListDetailfilter.as_view(), name='searchpost'),
+    # Post Admin 
+    path('admin/create/', CreatePost.as_view(), name='createposet'),
+    path('admin/edit/postdetail/<int:pk>', AdminPostDetail.as_view(), name='admindetailpost'),
+    path('admin/edit/<int:pk>', EditPost.as_view(), name='editpost'),
+    path('admin/delete/<int:pk>', DeletePost.as_view(), name='deletepost')
+   
 ]
