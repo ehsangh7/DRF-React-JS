f4b7114975bd4ad2d6a661b794cc6dfaf456a73b
unknown <ehsanghochagh@gamil.com>
2020-12-11 21:59:42 +0330

    create a search route in index.html

--- React/blogapi/src/index.js
+++ React/blogapi/src/index.js
@@ -4,17 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Route ,BrowserRouter as Router, Switch} from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import Register from './components/register';
 import Login from './components/login';
 import Logout from './components/logout';
 import Single from './components/single';
-
-
-
-
-
+import Search from './components/search'
 
 
 ReactDOM.render(
@@ -23,11 +19,11 @@ ReactDOM.render(
         <Header />
         <Switch>
           <Route exact path="/" component={App} />
-          <Route  path="/register" component={Register} />
-          <Route  path="/login" component={Login} />
-          <Route  path="/logout" component={Logout} />
-          <Route  path="/post/:slug" component={Single} />
-
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/post/:slug" component={Single} />
+          <Route path="/search" component={Search} />
         </Switch>
         <Footer />
     </React.StrictMode>
