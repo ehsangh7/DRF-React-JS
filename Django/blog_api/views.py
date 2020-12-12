80282cca30382029ade737f0f1554474903d5283
unknown <ehsanghochagh@gamil.com>
2020-12-12 14:41:42 +0330

    modified views.py

--- Django/blog_api/views.py
+++ Django/blog_api/views.py
@@ -1,66 +1,58 @@
-from rest_framework import generics
-from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import SAFE_METHODS, AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission, IsAdminUser, DjangoModelPermissions
-from rest_framework import viewsets
-from rest_framework import filters
-from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters, generics, permissions
 from rest_framework.response import Response
-from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.views import APIView
 
 # Display Posts
 
 class PostList(generics.ListAPIView):
+
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
-
 class PostDetail(generics.RetrieveAPIView):
+
     serializer_class = PostSerializer
 
-    def get_queryset(self):
+    def get_object(self, queryset=None, **kwargs):
         item = self.kwargs.get('pk')
         return get_object_or_404(Post, slug=item)
 
-# post Search
+# Post Search
 
 class PostListDetailfilter(generics.ListAPIView):
-
+  
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['^slug']
-
     # '^' Starts-with search.
     # '=' Exact matches.
-    # '@' Full-text search. (Currently only supported Django's PostgreSQL backend.)
-    # '$' Regex search.
+    search_fields = ['^slug']
 
+# Post Admin
 
-#  Post Admin 
-class CraetePost(generics.CreateAPIView):
-    permission_classes = [ permissions.IsAuthenticated ]
+class CreatePost(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class AdminPostDetail(generics.RetrieveAPIView):
-    permission_classes = [ permissions.IsAuthenticated ]
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class EditPost(generics.UpdateAPIView):
-    permission_classes = [ permissions.IsAuthenticated ]
-    queryset = Post.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = PostSerializer
-
-class DeletePost(generics.RetriveDestroyAPIView):
-    permission_classes = [ permissions.IsAuthenticated ]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
 
+class DeletePost(generics.RetrieveDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    
 
 """ Concrete View Classes
 # CreateAPIView
